f98344fec71ec9f32cde5edb2ac4005df071d9fe
Nicole <ngott30@gmail.com>
2023-01-05 19:02:32 +0100

    added php functionality for cover headline and adjusted colors and background image

--- index.php
+++ index.php
@@ -21,12 +21,28 @@
 
         .cover {
             background-color: #343434;
-            height: 30vh;
+            background-image: url('img/cover-articles.jpg');
+            background-size: cover;
+            height: 40vh;
+            width: 100%;
+            position: relative;
+        }
+
+        .cover h1 {
+            color: white;
+            /* position cover headline in the bottom right corner */
+            position: absolute;
+            right: 20px;
+            bottom: 10px;
+        }
+
+        .cover-pic {
+            height: 100%;
             width: 100%;
         }
 
         .menubar {
-            background-color: rgba(0,0,0, 0.2);
+            background-color: rgba(192,48,48, 0.7);
             text-align: right;
             padding: 20px;
             padding-right: 0;
@@ -34,27 +50,35 @@
         }
 
         .menubar a {
-            color: #343434;
+            color: white;
             text-decoration: none;
             padding: 20px;
         }
 
         .menubar a:hover {
-            background-color: rgba(0,0,0, 0.1);
-
+            background-color: rgba(192,48,48,0.2);
         }
     </style>
 </head>
 <body>
     <!-- Cover -->
     <div class="cover">
+        <?php
+            $headline = 'Blog Entries';
 
+            //Change Properties if Add Entry Page is Loaded
+            if($_GET['page'] == 'add') {
+                $headline = 'Add Blog Entry';
+            }
+
+            echo '<h1>' . $headline . '</h1>';
+        ?>
     </div>
 
     <!-- Menubar -->
     <div class="menubar">
-        <a href="">Articles</a>
-        <a href="">Add Articles</a>
+        <a href="index.php?page=articles">Articles</a>
+        <a href="index.php?page=add">Add Articles</a>
     </div>
 </body>
 </html>

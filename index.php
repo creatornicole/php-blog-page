7341cebaa866615925f4d63a3ee5857f0ed7286c
Nicole <ngott30@gmail.com>
2023-01-05 18:35:15 +0100

    created basics of html

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Blog</title>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <!-- Style -->
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+            background-color: #EAEDF8;
+            margin: 0;
+        }
+
+        .cover {
+            background-color: #343434;
+            height: 30vh;
+            width: 100%;
+        }
+
+        .menubar {
+            background-color: rgba(0,0,0, 0.2);
+            text-align: right;
+            padding: 20px;
+            padding-right: 0;
+            
+        }
+
+        .menubar a {
+            color: #343434;
+            text-decoration: none;
+            padding: 20px;
+        }
+
+        .menubar a:hover {
+            background-color: rgba(0,0,0, 0.1);
+
+        }
+    </style>
+</head>
+<body>
+    <!-- Cover -->
+    <div class="cover">
+
+    </div>
+
+    <!-- Menubar -->
+    <div class="menubar">
+        <a href="">Articles</a>
+        <a href="">Add Articles</a>
+    </div>
+</body>
+</html>
